fcb667063ff101699cee3c86275147cb03acdc57
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-06-13 11:11:52 -0400

    con models cambiado

--- src/categorias/models.py
+++ src/categorias/models.py
@@ -7,3 +7,39 @@ class Categoria(Base):
     id = Column(Integer, primary_key=True)
     nombre = Column(String, index=True)
     descripcion = Column(String, index=True)
+
+
+class Tipo_Usuario(Base): 
+    __tablename__  = "tipos_usuarios"
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
+
+class Tipo_Compra(Base): 
+    __tablename__  = "tipos_compras"
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
+
+class Estado_Cotizacion(Base): 
+    __tablename__  = "estados_cotizacion"
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
+
+class Metodo_Pago(Base): 
+    __tablename__  = "metodos_pagos"
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
+
+class Metodo_Envio(Base): 
+    __tablename__  = "metodos_envios"
+    
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, index=True)
+    descripcion = Column(String, index=True)
