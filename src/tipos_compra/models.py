b3677647a90c0da2b3ffe75a8962f6b08796f71b
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-06-13 18:03:50 -0400

    Con cambio de lugar

--- src/tipos_compra/models.py
+++ src/tipos_compra/models.py
@@ -8,17 +8,5 @@ class Tipo_Compra(Base):
     nombre = Column(String, index=True)
     descripcion = Column(String, index=True)
 
-class Tipo_Usuario(Base): 
-    __tablename__  = "tipos_usuarios"
-    
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String, index=True)
-    descripcion = Column(String, index=True)
 
-class Tipo_Producto(Base): 
-    __tablename__  = "tipos_productos"
-    
-    id = Column(Integer, primary_key=True)
-    nombre = Column(String, index=True)
-    descripcion = Column(String, index=True)
 
