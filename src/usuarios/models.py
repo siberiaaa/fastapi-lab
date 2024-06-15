5d9e9ef270c6f70bf601d81eea7082d950c5c132
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-06-14 23:32:45 -0400

    un cambio temporal de usuarios

--- src/usuarios/models.py
+++ src/usuarios/models.py
@@ -2,16 +2,16 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base #!aaaaaaa
 
-class Usuario(Base): 
-    __tablename__  = "usuarios"
+# class Usuario(Base): 
+#     __tablename__  = "usuarios"
     
-    cedula = Column(String, primary_key=True)
-    nombres = Column(String, index=True)
-    apellidos = Column(String, index=True)
-    nacimiento = Column(DateTime, index=True)
-    direccion = Column(String, index=True)
-    correo = Column(String, index=True)
-    contraseña = Column(String, index=True)
-    tipo_id = Column(Integer, ForeignKey("tipos_usuarios.id"))
+#     cedula = Column(String, primary_key=True)
+#     nombres = Column(String, index=True)
+#     apellidos = Column(String, index=True)
+#     nacimiento = Column(DateTime, index=True)
+#     direccion = Column(String, index=True)
+#     correo = Column(String, index=True)
+#     contraseña = Column(String, index=True)
+#     tipo_id = Column(Integer, ForeignKey("tipos_usuarios.id"))
 
-    tipo = relationship(back_populates='usuarios')
+#     tipo = relationship(back_populates='usuarios')
