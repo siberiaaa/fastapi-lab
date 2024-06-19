5edeeb0e6e2b61579578f5317e79db40efab3319
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-06-19 09:46:41 -0400

    Con un mínimo y máximo de estrellas

--- src/calificaciones/schemas.py
+++ src/calificaciones/schemas.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Union
-
 class CalificacionBase(BaseModel):
     titulo: str
     comentario: str
@@ -8,6 +7,14 @@ class CalificacionBase(BaseModel):
     emoticono: Union[str, None] = None
     usuario_cedula: str
     producto_id: int
+
+    @field_validator('estrellas')
+    def verificar_estrellas(cls, estrellas): 
+        if estrellas > 5: 
+            raise ValueError('El máximo de estrellas es 5')
+        if estrellas < 0: 
+            raise ValueError('El mínimo de estrellas es 0. ¡No seas cruel!')
+        return estrellas
 
 class CalificacionCrear(CalificacionBase):
     pass
