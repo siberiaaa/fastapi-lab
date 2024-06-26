54f7be50b45dafc33b08b9fdb8db5af34ab969f0
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-06-26 09:41:42 -0400

    Con obtener imagen y deshacer imagenes en creación de productos

--- static/js/index.js
+++ static/js/index.js
@@ -12,3 +12,26 @@ function consultar_producto(id){
     fetch('/productos/'+id,{method:'GET'})
     .then(location.reload(true));
 }
+
+function mostrar_imagen() {
+    var input = document.getElementById("extraccion");
+    var fReader = new FileReader();
+    fReader.readAsDataURL(input.files[0]);
+    fReader.onloadend = function(event) {
+        var img = document.getElementById("imagen");
+        img.value = event.target.result;
+        img = document.getElementById('externa')
+        img.src = event.target.result
+        console.log(event.target.result);
+        return event.target.result
+    }
+}
+
+function deimaginar() {
+    let imagen = document.getElementById('extraccion')
+    let externa = document.getElementById('externa')
+    let vistas = document.getElementById('imagen')
+    imagen.value = ''
+    externa.src = ''
+    vistas.value = ''
+}
